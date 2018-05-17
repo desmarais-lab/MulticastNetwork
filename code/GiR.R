a0391ebfa9e70750343015a79925c59158d37d24
bomin8319 <bomin8319>
2018-05-17 17:29:55 -0400

    ba-HEM.pdf

--- code/GiR.R
+++ code/GiR.R
@@ -2,7 +2,7 @@ source("/Users/bomin8319/Desktop/MulticastNetwork/code/Multicast.R")
 
 D = 100
 A = 5
-P = 5
+P = 4
 #Q = 3
 X = array(rnorm(D*A*A*P), dim = c(D,A,A,P))
 X[,,,1] = 1
@@ -19,12 +19,12 @@ support = gibbs.measure.support(A-1)
 prior.beta = list(mean = c(-3, rep(0, P-1)), var = diag(P))
 prior.eta = list(mean = rep(0, Q), var = diag(Q))
 prior.sigma2 = list(a = 3, b = 1)
-Nsamp = 500
-outer = 5
+Nsamp = 10^5
+outer = 10
 inner = c(5, 5, 1)
 burn = 0
 #Schein test
-result = matrix(NA, Nsamp, 2*(2+P+Q))
+result = matrix(NA, Nsamp, 2*(3+P+Q))
 for (n in 1:Nsamp) {
   if (n %% 100 == 0) print(n)
   beta = rmvnorm_arma(1, prior.beta$mean, prior.beta$var)
@@ -33,10 +33,13 @@ for (n in 1:Nsamp) {
   initial = Generate(D, A, beta, eta, sigma2, X, Y, support)
   infer = Inference(initial$data, X, Y, outer, inner, burn, prior.beta, prior.eta, prior.sigma2, 
                     initial = initial, proposal.var = c(0.01, 0.1, 0.01, 0.5), lasttime = 0)
-  result[n, ] = c(mean(vapply(initial$u, function(x) rowSums(x), rep(0, A))),
+  initial2 = Generate(D, A, infer$beta[outer,], infer$eta[outer,], infer$sigma2[outer,], X, Y, support)                  
+  result[n, ] = c(mean(vapply(initial$data, function(x) sum(x$r_d), c(1))),
+ 				 mean(vapply(2:D, function(d) initial$data[[d]]$t_d-initial$data[[d-1]]$t_d, c(1))),
                   initial$beta, initial$eta, initial$sigma2, 
-                  mean(vapply(infer$u, function(x) rowSums(x), rep(0, A))),
-                  infer$beta[outer,], infer$eta[outer,], infer$sigma2[outer,])			 
+                 mean(vapply(initial2$data, function(x) sum(x$r_d), c(1))),
+                  mean(vapply(2:D, function(d) initial2$data[[d]]$t_d-initial2$data[[d-1]]$t_d, c(1))),
+                  initial2$beta, initial2$eta, initial2$sigma2)			 
 }
 par(mfrow=c(2,5))
-GiR_PP_Plots(result[,c(1:(2+P+Q))], result[,c((3+P+Q):(2*(2+P+Q)))])
+GiR_PP_Plots(result[,c(1:(3+P+Q))], result[,c((4+P+Q):(2*(3+P+Q)))])
